fda3ffca1402622e5be087eca4430afa50c8e3db
Yz <yanzhen610@gmail.com>
2018-10-31 21:11:32 +0800

    simplify ex03.py

--- w08/ex03.py
+++ w08/ex03.py
@@ -2,28 +2,21 @@ a = ord('a')
 z = ord('z')
 
 while True:
-    op = input('(1) encode, (2) decode: ')
-    if op == '1':
-        offset = int(input('encode offset: '))
-        s = input('encode string: ')
-        for i in range(len(s)):
-            c = s[i]
-            if a <= ord(c) <= z:
-                c = chr((ord(c) - a + offset) % 26 + a)
-                print(c, end='')
-            else:
-                print(c, end='')
-    else:
-        offset = int(input('decode offset: '))
-        s = input('decode string: ')
-        for i in range(len(s)):
-            c = s[i]
-            if a <= ord(c) <= z:
-                c = chr((ord(c) - a - offset) % 26 + a)
-                print(c, end='')
-            else:
-                print(c, end='')
+    op = int(input('(1) encode, (2) decode: '))
+
+    offset = int(input('encode/decode offset: ')) * \
+            1 if op & 1 else -1
+    s = input('encode/decode string: ')
+    
+    for i in range(len(s)):
+        c = s[i]
+        if a <= ord(c) <= z:
+            c = chr((ord(c) - a + offset) % 26 + a)
+            print(c, end='')
+        else:
+            print(c, end='')
     print()
+
     cont = input('y or n: ')
     if cont != 'y':
         break
